b11ce951a5f0785c7ea3b4119c8f76397679e210
Christian Engel <christian.engel@wearekiss.com>
2012-11-16 11:22:49 +0100

    removed cachebuster for logic code

--- source/gears/initiator.js
+++ source/gears/initiator.js
@@ -5,7 +5,7 @@
  */
 
 require.config({
-    urlArgs:"bust=" + (new Date()).getTime(), //Remove after development
+    //urlArgs:"bust=" + (new Date()).getTime(), //Remove after development
     paths:{
         'collector_api':'http://social-rockstar.com/tide_collector/api/api.amd'
     }
